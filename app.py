e0d533a3e09a2bfad1297c959441cbfdb91c2048
Sasidar M <65358982+Sasi5526@users.noreply.github.com>
2021-06-27 20:52:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,68 +1,30 @@
-import pandas as pd
-import os
-from os  import getcwd
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
 import pickle
-from flask import Flask, render_template, request
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import pickle
 
-
+# load the model from disk
+filename = 'apriori_model.pkl'
+model = pickle.load(open(filename, 'rb'))
 app = Flask(__name__)
 
-directory = getcwd()
-
-
-# # Import the required Pickle files
-
-
-model = pickle.load(open(os.path.join(directory,'apriori_model.pkl', 'rb')))
-
-
-
-# This function structures the HTML code for displaying the table on website
-def html_code_table(df,table_name,file_name,side):
-    table_style = '<table style="border: 2px solid; float: ' + side + '; width: 40%;">'
-    table_head = '<caption style="text-align: center; caption-side: top; font-size: 140%; font-weight: bold; color:black;"><strong>' + table_name + '</strong></caption>'
-    table_head_row = '<tr><th>Recommendation</th></tr>'
-    
-    html_code = table_style + table_head + table_head_row
-    
-    for i in range(len(df.index)):
-        row = '<tr><td>' + str(df['Recommendation'][i]) +'</td></tr>'
-        html_code = html_code + row
-        
-    html_code = html_code + '</table>'
-    
-    file_path = os.path.join(directory,'templates/')
-    
-    hs = open(file_path + file_name + '.html', 'w')
-    
-    
-def apriori(prod_name):
-    apriori = model.loc[prod_name].sort_values(ascending=False)
-    
-    input_index = apriori[apriori['In Cart'] == prod_name].index
-    apriori.drop(index=input_index,inplace=True)
-    
-    apriori = model[~model['In Cart'].str.contains(prod_name, regex=False)].sort_values('Lift', ascending=False)[['Recommendation']].head(10)
-    
-    
-    html_code_table(apriori,'Recommendation of Next product in cart page is :','next_product','center')
-    
-
-@app.route("/")
+@app.route('/')
 def home():
+	return render_template('home.html')
 
-    return render_template('home.html')
+@app.route('/predict',methods=['POST'])
+def predict():
 
 
-@app.route("/login")
-def login():
+	if request.method == 'POST':
+		apriori = request.form['In Cart']
+		data = [apriori]
+		my_prediction = model[~model['In Cart'].str.contains(data, regex=False)].sort_values('Lift', ascending=False)[['Recommendation']].head(10)
+	return render_template('result.html',prediction = my_prediction)
 
-    prod_name = str(request.args.get('name')).upper()
-    
-    if prod_name in model['In Cart'].unique():
-        return render_template('cust_home.html',name=prod_name,new='n')
-    else:
-        return render_template('cust_home.html',name=prod_name,new='y')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(host='0.0.0.0',port=8080)
