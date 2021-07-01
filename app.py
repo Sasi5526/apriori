c6eabdbda757c44b7a767f3136bc794765bc96af
Sasidar M <65358982+Sasi5526@users.noreply.github.com>
2021-07-01 16:00:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,20 +10,31 @@ filename = 'apriori_model.pkl'
 model = pickle.load(open(filename, 'rb'))
 app = Flask(__name__)
 
+def get_recommendations(title):
+    
+    data = (title)
+    
+    return_df = model[~model['product_name'].str.contains(data, regex=False)].sort_values('Lift', ascending=False)[['Recommendation']]
+
+    return return_df
+
 @app.route('/')
 def home():
-	return render_template('home.html')
+	return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['post'])
 def predict():
 
 
-	if request.method == 'POST':
-		apriori = request.form['product_name']
-		data = [apriori]
-		my_prediction = model[~model['product_name'].str.contains(data, regex=False)].sort_values('Lift', ascending=False)[['Recommendation']].head(10)
-	return render_template('result.html',prediction = my_prediction)
+    if request.method == 'POST':
+        m_name = request.form['product_name']
+#        check = difflib.get_close_matches(m_name,all_titles,cutout=0.50,n=1)
+        result_final = get_recommendations(m_name)
+        names = []
+        for i in range(len(result_final)):
+            names.append(result_final.iloc[i][0])
+        return render_template('result.html',prediction=names)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
